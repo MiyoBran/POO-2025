e9e842e8871217eaf05c878708defc2f5ab6940a
Miyo Bran <miyenbran@gmail.com>
2025-08-16 16:15:48 -0300

    Add: Folder tp2EMpresa

---
+++ src/test/java/tp2Empresa/EmployeeTest.java
@@ -0,0 +1,24 @@
+package tp2Empresa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
